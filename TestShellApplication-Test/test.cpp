ffa28954f105bffe3fb63f7b05d332b6a10f9b13
DESKTOP-5FBE16T\User <user@user.com>
2024-06-04 14:48:45 +0900

    [feature][unittest] ssd device driver

--- TestShellApplication-Test/test.cpp
+++ TestShellApplication-Test/test.cpp
@@ -1,7 +1,32 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "SSDDriver.cpp"
+
+using namespace testing;
+
+class SsdDeviceDriverMock : public DriverInterface {
+public:
+	MOCK_METHOD(int, Read, (int lba), (override));
+	MOCK_METHOD(void, Write, (int lba, int dat), (override));
+};
+
+class TestShellApplicationTestFixture : public testing::Test {
+public:
+	SSDDriver ssdDriver;
+	SsdDeviceDriverMock ssdMock;
+
+protected:
+	void SetUp() override {
+		EXPECT_CALL(ssdMock, Read(_))
+			.WillRepeatedly(Return(0x0));
+
+		EXPECT_CALL(ssdMock, Write(_, _))
+			.WillRepeatedly(Return());
+	}
+};
+
+TEST_F(TestShellApplicationTestFixture, InterfaceTest) {
+	ssdDriver.Write(0, 0);
+	EXPECT_EQ(0x0, ssdDriver.Read(0));
 }
