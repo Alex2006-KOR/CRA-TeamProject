0a67ded7c21526198341665fed13b0abcb02e837
Alex2006 <ys88.lee@gmail.com>
2024-06-04 22:15:56 +0900

    [feature][ssddriver] Read Write Imeplementation

--- TestShellApplication/SSDDriver.cpp
+++ TestShellApplication/SSDDriver.cpp
@@ -1,10 +1,18 @@
 #include "SSDDriver.h"
+#include <cstdlib>
+#include <string>
+
+using namespace std;
 
 int SSDDriver::Read(int lba)
 {
-    return 0;
+    string command = "VirtualSSD.exe R " + to_string(lba);
+    int result = std::system(command.c_str());
+    return result;
 }
 
 void SSDDriver::Write(int lba, int data)
 {
+    string command = "VirtualSSD.exe W " + to_string(lba) + " " + to_string(lba);
+    int result = std::system(command.c_str());
 }
