4e87d8d6b925602933094e60e19c20cf48dcfbf2
DESKTOP-5FBE16T\User <user@user.com>
2024-06-04 17:13:39 +0900

    [feature][unittest] add invalid argument test

--- VirtualSSD-Test/DeviceDriver_test.cpp
+++ VirtualSSD-Test/DeviceDriver_test.cpp
@@ -3,6 +3,7 @@
 
 #include "ssd_interface.h"
 #include "DeviceDriver.cpp"
+#include "IOManager.cpp"
 
 using namespace testing;
 
@@ -16,22 +17,25 @@ class DeviceDriverTestFixture : public testing::Test {
 public:
 	SSDMock ssdMock;
 	DeviceDriver* pDeviceDriver = nullptr;
+	IOManager* pStIOManager = nullptr;
 
 protected:
 	// 초기화
 	virtual void SetUp() {
 		std::cout << "\tDeviceDriverTestFixture - SetUp" << std::endl;
 		pDeviceDriver = new DeviceDriver(&ssdMock);
+		//pStIOManager = new IOManager(pDeviceDriver);
 	}
 
 	// 정리
 	virtual void TearDown() {
 		std::cout << "\tDeviceDriverTestFixture - TearDown" << std::endl;
 		delete pDeviceDriver;
+		//delete pStIOManager;
 	}
 };
 
-TEST_F(DeviceDriverTestFixture, Read) {
+TEST_F(DeviceDriverTestFixture, SimpleRead) {
 	EXPECT_CALL(ssdMock, read)
 		.Times(1)
 		.WillOnce(Return(true));
@@ -41,12 +45,54 @@ TEST_F(DeviceDriverTestFixture, Read) {
 	pDeviceDriver->ReadData(0);
 }
 
-TEST_F(DeviceDriverTestFixture, ReadOverflowIndex) {
+TEST_F(DeviceDriverTestFixture, SimpleWrite) {
 	EXPECT_CALL(ssdMock, read)
-		.Times(0)
-		.WillOnce(Return(true));
+		.Times(0);
 	EXPECT_CALL(ssdMock, write)
-		.Times(1);
+		.Times(1)
+		.WillOnce(Return(true));
 
-	pDeviceDriver->WriteData(0, 0);
+	pDeviceDriver->WriteData(0, "0x00000000");
+}
+
+TEST_F(DeviceDriverTestFixture, InvalidCommand) {
+	EXPECT_CALL(ssdMock, read)
+		.Times(0);
+	EXPECT_CALL(ssdMock, write)
+		.Times(0);
+
+	try {
+		pStIOManager->DoCommand(string("Q"), 12, string("0x0000ABCD"));
+	}
+	catch (exception& e) {
+		FAIL();
+	}	
+}
+
+TEST_F(DeviceDriverTestFixture, WriteInvalidData) {
+	EXPECT_CALL(ssdMock, read)
+		.Times(0);
+	EXPECT_CALL(ssdMock, write)
+		.Times(0);
+
+	try {
+		pStIOManager->DoCommand("W", 0, "0xQWERZXCV");
+	}
+	catch (exception& e) {
+		FAIL();
+	}
+}
+
+TEST_F(DeviceDriverTestFixture, ReadInvalidData) {
+	EXPECT_CALL(ssdMock, read)
+		.Times(0);
+	EXPECT_CALL(ssdMock, write)
+		.Times(0);
+
+	try {
+		pStIOManager->DoCommand("R", 0, "0xQWERZXCV");
+	}
+	catch (exception& e) {
+		FAIL();
+	}
 }
