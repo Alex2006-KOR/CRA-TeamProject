591dc98c46f415e3a61bc21008c719429e87f4af
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-06-05 09:13:59 +0900

    [feat][ReadCommand] add exception unit test

--- TestShellApplication-Test/test.cpp
+++ TestShellApplication-Test/test.cpp
@@ -37,6 +37,50 @@ TEST_F(TestShellApplicationTestFixture, ReadZeroTest) {
 	EXPECT_EQ(output.str(), expected);
 }
 
+TEST_F(TestShellApplicationTestFixture, ReadExceptionTestInvalidChar) {
+	EXPECT_CALL(ssdMock, Read)
+		.Times(1)
+		.WillRepeatedly(Return(0x00000000));
+
+	Shell shell(&ssdMock);
+	std::istringstream input("read ABC\n");
+	std::ostringstream output;
+
+	EXPECT_THROW(shell.Run(input, output), invalid_argument);
+}
+
+TEST_F(TestShellApplicationTestFixture, ReadExceptionTestLBARangeExceeded) {
+	EXPECT_CALL(ssdMock, Read)
+		.Times(1)
+		.WillRepeatedly(Return(0x00000000));
+	std::string expected = "0x00000000\n";
+
+	Shell shell(&ssdMock);
+	std::istringstream input("read -10\n");
+	std::ostringstream output;
+
+	EXPECT_THROW(shell.Run(input, output), invalid_argument);
+}
+
+TEST_F(TestShellApplicationTestFixture, ReadReturnFormat) {
+	EXPECT_CALL(ssdMock, Read)
+		.Times(1)
+		.WillRepeatedly(Return(0x00000000));
+	std::string expected_prefix = "0x";
+	std::string expected_postfix = "\n";
+
+	Shell shell(&ssdMock);
+	std::istringstream input("read 10\n");
+	std::ostringstream output;
+
+	shell.Run(input, output);
+
+	string strOutput = output.str();
+
+	EXPECT_EQ(strOutput.substr(0, 2), expected_prefix);
+	EXPECT_EQ(strOutput.substr(strOutput.size()-1), expected_postfix);
+}
+
 TEST_F(TestShellApplicationTestFixture, WriteAndReadOnceTest) {
 	EXPECT_CALL(ssdMock, Write(10, 0xAA))
 		.Times(1);
