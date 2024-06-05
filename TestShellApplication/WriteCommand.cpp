Yuhaa/SSD_E

[refactoring][shell] WriteCommand::_parseCommand() -> extract method

TestShellApplication/WriteCommand.cpp
@@ -20,36 +20,46 @@ void WriteCommand::_execute()
 
 void WriteCommand::_parseCommand()
 {
-	if (m_commandList.size() != 2) {
+	constexpr int REQUIRED_COMMAND_COUNT = 2;
+	if (m_commandList.size() != REQUIRED_COMMAND_COUNT) {
 		throw std::exception();
 	}
 
-	string lbaString = m_commandList[0];
-	for (const char ch : lbaString) {
+	m_nLBA = _checkAndGetLBA(m_commandList[0]);
+	m_nData = _checkAndGetData(m_commandList[1]);
+}
+
+int WriteCommand::_checkAndGetLBA(string paramString)
+{
+	for (const char ch : paramString) {
 		if ((ch >= '0') && (ch <= '9')) continue;
 		throw std::exception("[Invalid LBA] Not Decimal Character");
 	}
-	int lba = std::stoi(lbaString, nullptr, 10);
-	if (lba < 0 || lba > 99) {
+	int result = std::stoi(paramString, nullptr, 10);
+	if (result < 0 || result > 99) {
 		throw std::exception("[Invalid LBA] LBA Must be 0 ~ 99");
 	}
-	m_nLBA = lba;
 
-	string dataString = m_commandList[1];
-	if (dataString.substr(0, 2).compare("0x") != 0) {
+	return result;
+}
+
+int WriteCommand::_checkAndGetData(string paramString)
+{
+	if (paramString.substr(0, 2).compare("0x") != 0) {
 		throw std::exception("[Invalid Data] Not Start 0x");
 	}
-	dataString = dataString.substr(2);
+	paramString = paramString.substr(2);
 
-	if (dataString.size() != 8) {
+	if (paramString.size() != 8) {
 		throw std::exception("[Invalid Data] Not 8 Character");
 	}
 
-	for (const char ch : dataString) {
+	for (const char ch : paramString) {
 		if ((ch >= '0') && (ch <= '9')) continue;
 		if ((ch >= 'a') && (ch <= 'f')) continue;
 		if ((ch >= 'A') && (ch <= 'F')) continue;
 		throw std::exception("[Invalid Data] Not Hex Character");
 	}
-	m_nData = std::stoi(dataString, nullptr, 16);
+
+	return std::stoi(paramString, nullptr, 16);
 }

Alex2006-KOR/CRA-TeamProject

TestShellApplication/WriteCommand.cpp
@@ -16,47 +16,59 @@ WriteCommand::WriteCommand(vector<string> commandList, DriverInterface* pSSDDriv
 
 bool WriteCommand::_parseCommand()
 {
-	if (m_vCommandList.size() != 2) {
+	constexpr int REQUIRED_COMMAND_COUNT = 2;
+	if (m_vCommandList.size() != REQUIRED_COMMAND_COUNT) {
 		m_out << "Invalid usage.\nCheck help message.\n";
 		return false;
 	}
 
-	string lbaString = m_vCommandList[0];
-	for (const char ch : lbaString) {
-		if ((ch >= '0') && (ch <= '9')) continue;
-		m_out << "INVALID LBA\n";
+	try {
+		m_nLBA = _checkAndGetLBA(m_vCommandList[0]);
+		m_nData = _checkAndGetData(m_vCommandList[1]);
+	}
+	catch (std::exception& e) {
+		m_out << e.what();
 		return false;
 	}
-	int lba = std::stoi(lbaString, nullptr, 10);
-	if (lba < 0 || lba > 99) {
-		m_out << "INVALID LBA\n";
-		return false;
-	}
-	m_nLBA = lba;
-
-	string dataString = m_vCommandList[1];
-	if (dataString.substr(0, 2).compare("0x") != 0) {
-		m_out << "INVALID DATA\n";
-		return false;
-	}
-	dataString = dataString.substr(2);
-
-	if (dataString.size() != 8) {
-		m_out << "INVALID DATA\n";
-		return false;
-	}
-
-	for (const char ch : dataString) {
-		if ((ch >= '0') && (ch <= '9')) continue;
-		if ((ch >= 'a') && (ch <= 'f')) continue;
-		if ((ch >= 'A') && (ch <= 'F')) continue;
-		m_out << "INVALID DATA\n";
-		return false;
-	}
-	m_nData = std::stoi(dataString, nullptr, 16);
 }
 
 void WriteCommand::_execute()
 {
 	m_pstSSDDriver->Write(m_nLBA, m_nData);
+}
+
+int WriteCommand::_checkAndGetLBA(string paramString)
+{
+	for (const char ch : paramString) {
+		if ((ch >= '0') && (ch <= '9')) continue;
+		throw std::exception("INVALID LBA\n");
+	}
+
+	int result = std::stoi(paramString, nullptr, 10);
+	if (result < 0 || result > 99) {
+		throw std::exception("INVALID LBA\n");
+	}
+
+	return result;
+}
+
+int WriteCommand::_checkAndGetData(string paramString)
+{
+	if (paramString.substr(0, 2).compare("0x") != 0) {
+		throw std::exception("INVALID DATA\n");
+	}
+	paramString = paramString.substr(2);
+
+	if (paramString.size() != 8) {
+		throw std::exception("INVALID DATA\n");
+	}
+
+	for (const char ch : paramString) {
+		if ((ch >= '0') && (ch <= '9')) continue;
+		if ((ch >= 'a') && (ch <= 'f')) continue;
+		if ((ch >= 'A') && (ch <= 'F')) continue;
+		throw std::exception("INVALID DATA\n");
+	}
+
+	return std::stoi(paramString, nullptr, 16);
 }