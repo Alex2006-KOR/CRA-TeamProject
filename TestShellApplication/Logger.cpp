f71dcecaf2cbc37c1b2c2868b44a166d76790a54
HyunMin Cho <hm1041.cho@samsung.com>
2024-06-07 11:34:52 +0900

    [refactoring][shell][log] Extract Method로 print 함수 정리
    print() 함수에 모두 구현되어 있던 코드를
    2가지 (message formating + print)로 분리하였음.
    
    그리고 Console 출력에는 포멧이 적용되지 않도록 수정.
    
    Signed-off-by: HyunMin Cho <hm1041.cho@samsung.com>

--- TestShellApplication/Logger.cpp
+++ TestShellApplication/Logger.cpp
@@ -7,28 +7,55 @@
 #include <fstream>
 
 
-void Logger::print(string msg, string strCallerName)
+void Logger::Print(string strMessage, string strCallerName)
+{
+    _printMessageToConsole(strMessage);
+    _printMessageToLogFile(strMessage, strCallerName);
+}
+
+void Logger::_printMessageToConsole(const string& strMessage)
+{
+    std::cout << strMessage << std::endl;
+}
+
+void Logger::_printMessageToLogFile(const string& strMessage, const string& strCallerName)
+{
+    const string LOG_FILE_NAME = "latest.log";
+
+    string formatMessage = _makeFormatMessage(strCallerName, strMessage);
+
+    std::ofstream ofs(".\\" + LOG_FILE_NAME, std::ios::app);
+    if (ofs.is_open() == false) {
+        throw std::exception("Failed to open log file.");
+    }
+    ofs << formatMessage << std::endl;
+    ofs.close();
+}
+
+string Logger::_makeFormatMessage(const string& strCallerName, const string& msg)
+{
+    return _getDateString() + " " + _getFormatMessage(strCallerName, msg);
+}
+
+string Logger::_getDateString()
 {
     std::time_t now = std::time(nullptr);
     std::tm localTime;
     errno_t err = localtime_s(&localTime, &now);
     std::ostringstream oss;
     oss << std::put_time(&localTime, "[%y.%m.%d %H:%M]");
-    string strDate = oss.str();
+
+    return oss.str();
+}
+
+string Logger::_getFormatMessage(string strCallerName, const string& msg)
+{
+    constexpr int FORMAT_MESAGE_SIZE = 30;
 
     std::ostringstream ossMessage;
     strCallerName += "()";
-    ossMessage << std::setw(30) << std::left << strCallerName;
+    ossMessage << std::setw(FORMAT_MESAGE_SIZE) << std::left << strCallerName;
     ossMessage << ": " << msg;
-    string strMessage = ossMessage.str();
 
-    string strTotalMessage = strDate + " " + strMessage;
-
-    std::cout << strTotalMessage << std::endl;
-    std::ofstream ofs(".\\latest.log", std::ios::app);
-    if (ofs.is_open() == false) {
-        throw std::exception("Failed to open log file.");
-    }
-    ofs << strTotalMessage << std::endl;
-    ofs.close();
+    return ossMessage.str();
 }
--- TestShellApplication/Logger.h
+++ TestShellApplication/Logger.h
@@ -10,5 +10,12 @@ public:
 	virtual ~Logger() = default;
 
 public:
-	void print(string msg, string strCallerName);
+	void Print(string strMessage, string strCallerName);
+
+private:
+	void _printMessageToConsole(const string& strMessage);
+	void _printMessageToLogFile(const string& strMessage, const string& strCallerName);
+	string _makeFormatMessage(const string& strCallerName, const string& msg);
+	string _getDateString();
+	string _getFormatMessage(string strCallerName, const string& msg);
 };
