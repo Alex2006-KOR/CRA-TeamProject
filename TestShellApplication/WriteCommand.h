2ff0a1b804a419c5502705cd3eac5a10f5b9c921
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-06-07 10:37:46 +0900

    [refactor][WriteCommand] change to use exception & method chaining, not outstream

--- TestShellApplication/WriteCommand.h
+++ TestShellApplication/WriteCommand.h
@@ -1,24 +1,33 @@
 #pragma once
-#include "SSDCommand.h"
 #include "DriverInterface.h"
+#include <vector>
+
+using namespace std;
 
 class WriteCommand
-	: public BaseSSDCommand
 {
 public:
-	WriteCommand(DriverInterface* pSSDDriver, std::ostream& output);
+	WriteCommand(DriverInterface* pstDriver);
+	WriteCommand& setLBA(string strLba);
+	WriteCommand& setData(string strData);
+	void execute();
 
-protected:
-	bool _parseCommand() override;
-	void _execute() override;
-
-private:
-	int _checkAndGetLBA(string paramString);
-	int _checkAndGetData(string paramString);
+	bool CheckArgCnt(vector<string> vArgs) const;
 
 private:
-	DriverInterface* m_pstSSDDriver;
-
+	DriverInterface* m_pstDriver;
+	vector<string> m_vArguemnts;
 	int m_nLBA;
 	int m_nData;
+
+	const int REQUIRED_COMMAND_COUNT = 2;
+	const int REQUIRED_DATA_LENGTH = 10;
+
+	void _checkValidLBAFormat(string strLBA);
+	void _updateLBA(string strLBA);
+	void _checkLBARange();
+
+	void _updateData(std::string& strData);
+	void _checkSpelling(std::string& strData);
+	void _checkDataFormat(std::string& strData) const;
 };
