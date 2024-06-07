298a65c63ed0e44da7d9a83a0f2ece1b5f21c5cd
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-06-07 17:07:51 +0900

    [feat][TestScript] add Test Script Class

---
+++ TestShellApplication/TestScripts/TestApp2.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "TestScriptBase.h"
+
+class TestApp2 : public TestScriptBase {
+public:
+	TestApp2(DriverInterface* pstDriver, ostream& output);
+	void _setup() override;
+	void _main() override;
+	void _cleanup() override;
+
+private:
+	string m_strDataPrev;
+	string m_strDataNew;
+	const int AGING_CNT = 30;
+};
