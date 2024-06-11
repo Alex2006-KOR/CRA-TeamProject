7e90187dcb516cbdf089c158b62f911e28d8ca1c
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-06-11 10:10:43 +0900

    [fix][Help] help message에 잘못 나온 값 수정합니다. (erase command에서 block count 0은 지원하지 않음)

--- TestShellApplication/Shell.cpp
+++ TestShellApplication/Shell.cpp
@@ -69,7 +69,7 @@ void Shell::_printHelp()
 \n\
 [lba] : decimal only, range = [0, 99]\n\
 [data] : hexadecimal only, range = [0x00000000, 0xFFFFFFFF]\n\
-[block count] : decimal only, range = [0, 10] (erase) \n\
+[block count] : decimal only, range = [1, 10] (erase) \n\
 ";
     LOG(strHelp);
 }
